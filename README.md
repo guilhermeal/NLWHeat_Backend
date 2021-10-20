7c4f7ce5320f9b7c006cf8e0c87434b2f025f164
Guilherme Almeida <guiallan.al@gmail.com>
2021-10-20 02:32:01 -0300

    ⚡ chore: configure authenticate using github

--- README.md
+++ README.md
@@ -31,7 +31,9 @@ Create `.env` on root and set:
 ```
 GITHUB_CLIENT_SECRET=<put-here-your-cliente-secret-key>
 GITHUB_CLIENT_ID=<put-here-your-cliente-id>
-```
-This file must content your database settings & Github OAuth.
 
+JWT_SECRET=<any-hash-or-name>
+```
+This file must content your settings of Github OAuth and JWT_SECRET
 GitHub OAuth Generator: `https://github.com/settings/developers`
+THe `JWT_SECRET` used to generete your token after login.
--- controllers/AuthenticateUserController.ts
+++ controllers/AuthenticateUserController.ts
@@ -6,9 +6,13 @@ class AuthtenticateUserController {
 
         const { code } = request.body;
         const service = new AuthtenticateUserService();
-        const result = await service.execute(code);
 
-        return response.json(result);
+        try {
+            const result = await service.execute(code);
+            return response.json(result);
+        } catch(err) {
+            return response.json({ error: err.message });
+        }
     }
 }
 
