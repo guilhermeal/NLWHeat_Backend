63c3e8cba789c18a782dae53cbac9d6121f19730
Guilherme Almeida <guiallan.al@gmail.com>
2021-10-20 04:27:33 -0300

    ⚡ chore(messages): return last 3 messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { AuthtenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { GetLast3MessagesService } from "./services/GetLast3MessagesService";
 
 const router = Router();
 
@@ -11,5 +13,9 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+
+
 
 export { router };
