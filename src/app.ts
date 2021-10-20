2268f85677bfab26146029de913191a67a29be1f
Guilherme Almeida <guiallan.al@gmail.com>
2021-10-20 04:16:28 -0300

    ⚡ chore(message): configure websocket

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import "dotenv/config";
 import express from "express";
-
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 import { router } from "./routes";
-const app = express();
-app.use(express.json());
 
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
 app.use(router);
 
 app.get("/github", (request, response) => {
@@ -19,4 +34,4 @@ app.get("/signin/callback", (request, response) => {
     return response.json(code);
 });
 
-app.listen(4000, () => console.log(`✨ Server is running on Port 4000`));
+export { serverHttp, io };
